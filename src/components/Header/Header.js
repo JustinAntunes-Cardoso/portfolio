c17cec3be88eb9457d39e049a10a16f57e602ca9
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-04-07 14:35:40 -0400

    Created and styled Header Component. Added links to future sections and transitions. Removed test and css files.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Header.scss';
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<header className='header'>
+			<a className='header__logo' href='#home'>Justin.dev</a>
+			<nav className='header__nav'>
+				<ul className='header__list'>
+          <li className='header__list-item'><a className='header__link' href='#home'>Home</a></li>
+          <li className='header__list-item'><a className='header__link' href='#about'>About</a></li>
+          <li className='header__list-item'><a className='header__link' href='#projects'>Projects</a></li>
+          <li className='header__list-item'><a className='header__link' href='#contact'>Contact</a></li>
+        </ul>
+			</nav>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
