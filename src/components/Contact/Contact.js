01c21129857b0b08830eb99e132bc0c9b744d61f
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-04-15 19:38:32 -0400

    Added contact section to site with mobile styling & slight changes to other sections formatting.

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Contact.scss';
+
+function Contact() {
+	return (
+		<section
+			id='contact'
+			className='contact'>
+			<article className='contact__title-container'>
+				<h3 className='contact__title'>CONTACT</h3>
+				<p className='contact__text'>
+					feel free to contact me at my contact information below. 📞📱📧💬📫
+				</p>
+			</article>
+			<aside className='contact__icons-container'>
+				<div className='contact__info'>
+					<div className='contact__image-container'>
+						<i className='contact__image fa-solid fa-map-location-dot'></i>
+					</div>
+					<div className='contact__info-container'>
+						<h6 className='contact__info-title'>Location</h6>
+						<p className='contact__info-text'>Mississauga, Canada</p>
+					</div>
+				</div>
+				<div className='contact__info'>
+					<div className='contact__image-container'>
+						<i className='contact__image fa-solid fa-envelope-open-text'></i>
+					</div>
+					<div className='contact__info-container'>
+						<h6 className='contact__info-title'>Mail</h6>
+						<p className='contact__info-text'>
+							justin.antunes-cardoso@hotmail.com
+						</p>
+					</div>
+				</div>
+			</aside>
+		</section>
+	);
+}
+
+export default Contact;
