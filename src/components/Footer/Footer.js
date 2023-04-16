60501510853341f4a8e7ecff7cecfbbd72a94421
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-04-16 08:57:40 -0400

    Footer added to the site with mobile styling. Contact section style was slightly modified.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const year = new Date().getFullYear();
 
-export default Footer
+	return (
+		<footer className='footer'>
+			<div className='footer__info-container'>
+				<p className='footer__info'>{`Copyright © ${year}. All rights are reserved`}</p>
+			</div>
+			<div class='footer__link-container'>
+				<a
+					aria-label='linkedin'
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.linkedin.com/in/justin-antunes-cardoso/   '>
+					<i class='footer__link fa-brands fa-linkedin'></i>
+				</a>
+				<a
+					aria-label='github'
+					target='_blank'
+					rel='noreferrer'
+					href='https://github.com/JustinAntunes-Cardoso'>
+					<i class='footer__link fa-brands fa-github'></i>
+				</a>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
