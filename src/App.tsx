7c03db062210cfc553b57ac9500e62efc30e9370
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-17 17:24:08 -0400

    Create Certification Section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.scss';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Certifications from './components/Certifications/Certifications';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Certifications />
       <Projects />
       <Contact />
       <Footer />
