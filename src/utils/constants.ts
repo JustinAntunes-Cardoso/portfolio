2a54bd2d6436b68a591016fb781b3a3b9bcd1c10
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-29 19:46:02 -0400

    Added aws badge

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -58,6 +58,11 @@ export const badges: Badge[] = [
 	},
 	{
 		src: '//cdn.credly.com/assets/utilities/embed.js',
+		badgeId: '919e366e-b585-411a-9fe8-9795654c3ed9',
+		badgeHost: 'https://www.credly.com',
+	},
+	{
+		src: '//cdn.credly.com/assets/utilities/embed.js',
 		badgeId: '7cf24c39-eee5-443c-ae7a-8f5d4c22fa04',
 		badgeHost: 'https://www.credly.com',
 	},
