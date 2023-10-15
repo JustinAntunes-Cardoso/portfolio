d15b1f56dfc08fb8eed79bba08001823c4ac604f
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-14 23:14:26 -0400

    Create routes for different career paths

---
+++ src/pages/Developer/Developer.tsx
@@ -0,0 +1,23 @@
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero';
+import About from '../../components/About/About';
+import Certifications from '../../components/Certifications/Certifications';
+import Projects from '../../components/Projects/Projects';
+import Contact from '../../components/Contact/Contact';
+import Footer from '../../components/Footer/Footer';
+
+const Developer = () => {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <About />
+            <Certifications />
+            <Projects />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default Developer
