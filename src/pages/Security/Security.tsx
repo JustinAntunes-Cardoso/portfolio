7ee9031f73870d93206e27b3ef05aabf8359703e
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-15 20:11:05 -0400

    Init Security page components

--- src/pages/Security/Security.tsx
+++ src/pages/Security/Security.tsx
@@ -1,8 +1,21 @@
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero';
+import About from '../../components/About/About';
+import Certifications from '../../components/Certifications/Certifications';
+import Projects from '../../components/Projects/Projects';
+import Contact from '../../components/Contact/Contact';
+import Footer from '../../components/Footer/Footer';
 
 const Security = () => {
     return (
         <>
-
+            <Header />
+            <Hero />
+            <About />
+            <Certifications />
+            <Projects />
+            <Contact />
+            <Footer />
         </>
     )
 }
