d09a66a7be15b77248f6ce5df3dd59ea70b92ae6
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-19 19:09:56 -0400

    Added props to Dev and Cyber

--- src/pages/Security/Security.tsx
+++ src/pages/Security/Security.tsx
@@ -1,21 +1,22 @@
-import Header from '../../components/Header/Header'
-import Hero from '../../components/Hero/Hero';
-import About from '../../components/About/About';
-import Certifications from '../../components/Certifications/Certifications';
-import Projects from '../../components/Projects/Projects';
-import Contact from '../../components/Contact/Contact';
-import Footer from '../../components/Footer/Footer';
+// import Header from '../../components/Header/Header'
+// import Hero from '../../components/Hero/Hero';
+// import About from '../../components/About/About';
+// import Certifications from '../../components/Certifications/Certifications';
+// import Projects from '../../components/Projects/Projects';
+// import Contact from '../../components/Contact/Contact';
+// import Footer from '../../components/Footer/Footer';
+// import { cyber } from '../../utils/cybersecurity';
 
 const Security = () => {
     return (
         <>
-            <Header />
-            <Hero />
-            <About />
-            <Certifications />
-            <Projects />
+            {/* <Header />
+            <Hero data={cyber.data.hero} />
+            <About data={cyber.data.about} />
+            <Certifications data={cyber.data.certifications} />
+            <Projects data={cyber.data.projects} />
             <Contact />
-            <Footer />
+            <Footer /> */}
         </>
     )
 }
