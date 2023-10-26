6812db03db90f0c6e272d499fa32b00967009b15
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-25 22:58:02 -0400

    Add project props and type

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,14 +1,19 @@
 import ProjectList from '../ProjectList';
 import './Projects.scss';
+import { Project } from '../../utils/types';
 
-const Projects = () => {
+interface ProjectsProps {
+	projects: Project[];
+}
+
+const Projects: React.FC<ProjectsProps> = ({ projects }) => {
 	//🍷🥂
 	return <section id='projects' className='projects'>
 		<div className='projects__title-container'>
 			<h3 className='projects__title'>Portfolio</h3>
 			<h1 className='projects__text'>Each Project is a Bespoke Piece of Work 💼</h1>
 		</div>
-		<ProjectList />
+		<ProjectList projects={projects} />
 	</section>;
 };
 
